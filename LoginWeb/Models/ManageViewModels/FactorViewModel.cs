7fc704430eb96405bb8451fc48ec02ec6bd83045
champ <champ6803@gmail.com>
2018-07-13 15:08:01 +0700

    Project Start

---
+++ LoginWeb/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginWeb.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
