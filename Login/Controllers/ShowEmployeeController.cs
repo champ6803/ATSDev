653d827472e76ca889e9635e940a71d394a8ae42
jiidapa <jidapa.p@kkumail.com>
2018-07-20 14:47:49 +0700

    comm

--- Login/Controllers/ShowEmployeeController.cs
+++ Login/Controllers/ShowEmployeeController.cs
@@ -5,22 +5,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Login.Models;
+using Login.UserModel;
+using System.Net.Http;
+using System.Net.HttpCilent;
+using Newtonsoft.Json;
+
 
 namespace Login.Controllers
 {
     public class ShowEmployeeController : Controller
     {
+        HttpCilent cilent = new HttpCilent();
         public IActionResult ShowEmployee()
         {
             return View();
         }
-         public ActionResult Index(string e) 
+         public UserRepo() 
         {
-            
-            // WebClient client = new WebClient();
-            // string url = dashboardUrlBase + "GetUserRecord?userName=" + adSAMName;
-            // User result = JsonConvert.DeserializeObject<User>(client.DownloadString(url));
-            return View();
+            cilent.BaseAddress = new Url("https://localhost:5001/");
+            cilent.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderVaalue("application/json")
+            );
+        }
+
+        public async Task<List<UserModel>> GetAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(3000);
+            cancellationToken.ThrowIfCancellationToken();
+
+            HttpResponseMessage response = await cilent.GetAsync("api/generateUser", cancellationToken);
+            if(response.IsSuccessStatusCode)
+            {
+                var result =  await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject <List<UserModel>>(result);
+            }
+                return new <List<UserModel>>();
+            return null;
         }
     }
 }
---
+++ Login/Models/UserModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Login.UserModel
+{
+    public class User
+    {
+        public string email { get; set;}
+        public string name {get; set;}
+        public string id {get; set;}
+    }
+}
