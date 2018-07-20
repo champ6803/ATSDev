5470ce9563a45296727aed53859a8ef73a1b15a6
champ <champ6803@gmail.com>
2018-07-20 13:32:07 +0700

    commit

--- Login/wwwroot/js/showEmployeeController.js
+++ Login/wwwroot/js/showEmployeeController.js
@@ -1,22 +1,30 @@
 var app = angular.module("myapp", []); 
-app.controller("ShowEmployeeController", function($scope , $http){
+app.config(function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  });
+
+app.service('dataService', function($http) {
+    delete $http.defaults.headers.common['X-Requested-With'];
+    this.flickrPhotoSearch = function() {
+        return $http({
+            method: 'GET',
+            url: 'https://localhost:5001/api/values',
+            dataType: 'jsonp',
+            headers: {'Authorization': 'Token token=xxxxYYYYZzzz'}
+         });
+     }
+});
+
+app.controller("ShowEmployeeController", function($scope , $http , dataService){
 
     $scope.employees = typeof employees != "undefined" ? employees ? employees : null : null;
 
 $scope.init= function(){
     debugger;
-    var req ={
-        url=""
-    }
-    $http.get(req).then(function(response){
-        if(response.data){
-            alert('ดึงรายชื่อสำเร็จ');
-            $scope.employees = response.data;
-        }else{
-            alert('ดึงรายชื่อไม่สำเร็จ');
-        }
-    },function(response){
-            alert('fail');
+    dataService.flickrPhotoSearch().then(function(dataResponse) {
+        $scope.employees = dataResponse;
+        console.log($scope.employees);
     });
 }
 });
